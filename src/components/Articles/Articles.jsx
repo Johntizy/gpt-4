11d4e4febf9602d829fa351c8fbb156f783485ab
unknown <jtaiwo909@gmail.com>
2023-07-19 01:26:25 +0100

    Blog section completed

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Articles.css';
 
-const Articles = () => {
+const Articles = ({imgUrl, title, date}) => {
   return (
-    <div>Articles</div>
+    <div className='blog_container_article'>
+      <div className="blog_container_article_image">
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="blog_container_article_content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { Articles } from '../../components';
+import {blog01, blog02, blog03, blog04, blog05} from './import';
+import './Blog.css';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='blog section_padding' id='blogs'>
+      <div className="blog_heading">
+        <h1 className='gradient_text'>
+        A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="blog_container">
+        <div className="blog_container_groupA">
+          <Articles 
+            imgUrl={blog01} 
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            text="Feb 08, 2023"
+          />
+        </div>
+        <div className="blog_container_groupB">
+        <Articles 
+            imgUrl={blog02} 
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            text="Jul 03, 2023"
+          />
+          <Articles 
+            imgUrl={blog03} 
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            text="Aug 09, 2023"
+          />
+          <Articles 
+            imgUrl={blog04} 
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            text="Nov 03, 2023"
+          />
+          <Articles 
+            imgUrl={blog05} 
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            text="Nov 12, 2023"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
